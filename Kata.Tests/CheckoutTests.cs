ec9c857f9880fc8d2ccbd1a801b1e11c5438dcfd
Anastasia Kerr <anastasia.kerr@leacam.ie>
2024-03-18 20:15:09 +0000

    Scan_WhenItemsScanned_AddItemsToCart

--- Kata.Tests/CheckoutTests.cs
+++ Kata.Tests/CheckoutTests.cs
@@ -5,6 +5,24 @@ namespace Kata.Tests;
 public class CheckoutTests
 {
     [Test]
+    public void Scan_WhenItemsScanned_AddItemsToCart()
+    {
+
+        var mockCart = new Mock<ICart>();
+
+        ICheckout checkout = new Checkout(mockCart.Object);
+
+        checkout.Scan("A");
+        checkout.Scan("B");
+        checkout.Scan("A");
+        checkout.Scan("B");
+        checkout.Scan("A");
+
+        mockCart.Verify(m => m.AddItem("A"), Times.Exactly(3));
+        mockCart.Verify(m => m.AddItem("B"), Times.Exactly(2));
+    }
+
+    [Test]
     public void GetTotalPrice_WhenItemsScanned_AddItemsToCart()
     {
 
