fc3bc1a38be274d21ba13e14d9cd9f6a5acf9470
Anastasia Kerr <anastasia.kerr@leacam.ie>
2024-03-18 21:05:41 +0000

    more tests

--- Kata.Tests/CheckoutTests.cs
+++ Kata.Tests/CheckoutTests.cs
@@ -31,15 +31,62 @@ public class CheckoutTests
     }
 
     [Test]
+    public void Scan_WhenSingleItemScannedMultipleTimes_AddsItemToCartCorrectly()
+    {
+        var mockCart = new Mock<ICart>();
+        ICheckout checkout = new Checkout(mockCart.Object, pricingRules);
+
+        checkout.Scan("D");
+        checkout.Scan("D");
+        checkout.Scan("D");
+
+        mockCart.Verify(m => m.AddItem("D"), Times.Exactly(3));
+    }
+
+    [Test]
     public void GetTotalPrice_WhenMultipleItemsScanned_CalculatesSpecialPriceCorrectly()
     {
 
-       // var mockCart = new Mock<ICart>();
-        var checkout = new Checkout(new SimpleCart(), pricingRules);
-        checkout.Scan("B");
-        checkout.Scan("A");
-        checkout.Scan("B");
+        var mockCart = new Mock<ICart>();
+        mockCart.Setup(m => m.GetItems()).Returns(new Dictionary<string, int>
+            {
+                { "A", 1 },
+                { "B", 2 }
+            });
+
+        var checkout = new Checkout(mockCart.Object, pricingRules);
 
         Assert.That(checkout.GetTotalPrice(), Is.EqualTo(95));
     }
+
+    [Test]
+    public void GetTotalPrice_WhenItemsWithoutSpecialPricesScanned_CalculatesTotalCorrectly()
+    {
+        var mockCart = new Mock<ICart>();
+        mockCart.Setup(m => m.GetItems()).Returns(new Dictionary<string, int>
+    {
+        { "C", 2 },
+        { "D", 3 }
+    });
+
+        var checkout = new Checkout(mockCart.Object, pricingRules);
+
+        Assert.That(checkout.GetTotalPrice(), Is.EqualTo(150)); // C: 2*30, D: 3*30
+    }
+
+    [Test]
+    public void GetTotalPrice_WhenItemsWithSpecialPricesExceedingRequiredQuantityScanned_CalculatesTotalCorrectly()
+    {
+        var mockCart = new Mock<ICart>();
+        mockCart.Setup(m => m.GetItems()).Returns(new Dictionary<string, int>
+    {
+        { "A", 7 }, // Special price for 3, so 2*130 for 6, plus 1*50 for the remaining 1
+        { "B", 5 }  // Special price for 2, so 2*45 for 4, plus 1*30 for the remaining 1
+    });
+
+        var checkout = new Checkout(mockCart.Object, pricingRules);
+
+        Assert.That(checkout.GetTotalPrice(), Is.EqualTo(130 * 2 + 50 + 45 * 2 + 30)); // 390
+    }
+
 }
